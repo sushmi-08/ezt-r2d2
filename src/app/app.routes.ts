5107650342d49649ca0b7aa73ca7c5fd90873f76
sushmitha-yoegndra_mcgraw <sushmitha.yogendra@mheducation.com>
2025-06-09 15:11:00 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { IntroComponent } from './pods/activity/intro/intro.component';
 // import { bootstrapGuard } from './pods/bootstrap/bootstrap.guard';
 // import { entryPointGuard } from './pods/entry-point/entry-point.guard';
 import { ErrorComponent } from './pods/error/error.component';
+import { QuestionGroupComponent } from './pods/question-group/question-group.component';
+import { QuestionComponent } from './pods/question-group/question/question.component';
 import { QuestionNavigationComponent } from './pods/question-navigation/question-navigation.component';
 
 export const routes: Routes = [
@@ -19,6 +21,14 @@ export const routes: Routes = [
   //   path: 'lang/:locale/eaid/:eaid',
   //   canActivate: [entryPointGuard],
   // },
+  {
+    path: 'bootstrap/:id',
+    redirectTo: 'activity',
+  },
+  {
+    path: 'lang/:locale/eaid/:eaid',
+    redirectTo: 'activity',
+  },
   {
     path: 'activity',
     component: ActivityComponent,
@@ -39,6 +49,16 @@ export const routes: Routes = [
         path: 'activity-completed',
         component: ActivityCompletedComponent,
       },
+      {
+        path: ':questionGroupId',
+        component: QuestionGroupComponent,
+        children: [
+          {
+            path: ':questionId',
+            component: QuestionComponent,
+          },
+        ],
+      },
     ],
   },
   {
@@ -54,3 +74,4 @@ export const routes: Routes = [
     redirectTo: 'activity',
   },
 ];
+ 
